9f5b8e3a4c4220e3b82f5085026e1fead01947b2
Diego Trazzi <diegotrazzi@gmail.com>
2025-04-30 19:14:34 -0700

    Input

--- WordleSolver.cpp
+++ WordleSolver.cpp
@@ -5,3 +5,54 @@
 #include <sstream>
 #include <algorithm>
 #include <iterator>
+
+class WordleSolver
+{
+
+private:
+    std::vector<std::string> ignoreWords;
+    std::vector<std::string> board;
+
+public:
+    void display(const std::vector<std::string> currentBoard)
+    {
+        std::cout << "[";
+        for (size_t i = 0; i < currentBoard.size(); ++i)
+        {
+            std::cout << currentBoard[i];
+            if (i < currentBoard.size() - 1)
+            {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "]";
+    }
+
+    void resetBoard()
+    {
+        board.clear();
+        for (int i = 0; i < 5; ++i)
+        {
+            board.push_back("_");
+        }
+    }
+
+    std::string getInput(std::string prompt) {
+        std::cout << prompt << " ";
+        std::string input;
+        std::getline(std::cin, input);
+        return input;
+    }
+
+    void play() {
+        std::cout << getInput("Enter some text: ") << std::endl;
+    }
+
+};
+
+int main() {
+    WordleSolver solver;
+    solver.play();
+
+    return 0;
+}
